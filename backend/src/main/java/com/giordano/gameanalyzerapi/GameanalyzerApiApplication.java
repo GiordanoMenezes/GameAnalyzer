296a8c8d98645096a5ebc5fea2cd5d666649ceb8
Giordano Menezes <giordano.menezes@bol.com.br>
2020-09-15 19:47:53 -0300

    Project Created

---
+++ backend/src/main/java/com/giordano/gameanalyzerapi/GameanalyzerApiApplication.java
@@ -0,0 +1,13 @@
+package com.giordano.gameanalyzerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameanalyzerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameanalyzerApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/giordano/gameanalyzerapi/GameanalyzerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giordano.gameanalyzerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameanalyzerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

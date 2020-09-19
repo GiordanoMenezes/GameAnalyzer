641b426b217126a2cd7adac8cf9bba98d09e29a9
Giordano Menezes <giordano.menezes@bol.com.br>
2020-09-19 00:50:33 -0300

    Page Home created

---
+++ frontweb/src/pages/Home/Home.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { ReactComponent as ArrowRight } from '../../assets/images/arrowright.svg';
+import { ReactComponent as GamerImg } from '../../assets/images/gamer.svg';
+
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="home-text">
+        <h1 className="home-text-title">
+          Quais jogos a galera gosta mais?
+        </h1>
+        <h3 className="home-text-subtitle">
+          Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo
+        </h3>
+        <div className="home-actions">
+          <button className="home-btn">Quero Saber quais são</button>
+          <div className="home-btn-icon">
+            <ArrowRight />
+          </div>
+        </div>
+      </div>
+      <GamerImg className="home-image" />
+    </div>
+  );
+}
+
+export default Home;
